4a062b4c38ea4ed920d6e22ebf560b38385b878e
Barracuda72 <barracuda72@bk.ru>
2024-07-01 22:25:16 +0500

    Ignore EEXIST errors when populating initramfs

--- init/noinitramfs.c
+++ init/noinitramfs.c
@@ -35,23 +35,23 @@ int __init default_rootfs(void)
 	int err;
 
 	err = sys_mkdir((const char __user __force *) "/dev", 0755);
-	if (err < 0)
+	if (err < 0 && err != -EEXIST)
 		goto out;
 
 	err = sys_mknod((const char __user __force *) "/dev/console",
 			S_IFCHR | S_IRUSR | S_IWUSR,
 			new_encode_dev(MKDEV(5, 1)));
-	if (err < 0)
+	if (err < 0 && err != -EEXIST)
 		goto out;
 
 	err = sys_mkdir((const char __user __force *) "/root", 0700);
-	if (err < 0)
+	if (err < 0 && err != -EEXIST)
 		goto out;
 
 	return 0;
 
 out:
-	printk(KERN_WARNING "Failed to create a rootfs\n");
+	printk(KERN_WARNING "Failed to create a rootfs: %d\n", err);
 	return err;
 }
 #if !IS_BUILTIN(CONFIG_BLK_DEV_INITRD)
